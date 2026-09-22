b531a294dfa6674470e1dabbe9b4429b5e42c5c9
teoryman143 <jonathancortestm143@gmail.com>
2026-09-22 08:40:18 -0500

    feat: Synchronize game methods and update server socket initialization

--- server/src/main/java/co/icesi/buscaminas/model/BoardGame.java
+++ server/src/main/java/co/icesi/buscaminas/model/BoardGame.java
@@ -12,7 +12,7 @@ public class BoardGame {
         return mines;
     }
 
-    public int initGame(int n, int m, int mines){
+    public synchronized int initGame(int n, int m, int mines){
         this.mines = mines;
         board = new Cell[n][m];
         Random rd = new Random();
@@ -42,7 +42,7 @@ public class BoardGame {
         return mi;
     }
 
-    public void showAll(boolean show){
+    public synchronized void showAll(boolean show){
         for (int i = 0; i <board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 board[i][j].setShowAll(show);
@@ -78,7 +78,7 @@ public class BoardGame {
             System.out.println(" ]");
         }
     }
-    public boolean selectCell(int i, int j){
+    public synchronized boolean selectCell(int i, int j){
         if(i<0 || i>= board.length || j<0 || j >= board[0].length ){
             throw new RuntimeException("Cell no valid");
         }
@@ -104,7 +104,7 @@ public class BoardGame {
         return win;
     }
 
-    private void showCells(int i, int j, boolean deep) {
+    private synchronized void showCells(int i, int j, boolean deep) {
         if(i<0 || i>= board.length || j<0 || j >= board[0].length || !board[i][j].isHide()){
             return;
         }
@@ -129,7 +129,7 @@ public class BoardGame {
         return board;
     }
 
-    public void markCell(int i, int j) {
+    public synchronized void markCell(int i, int j) {
         if(i<0 || i>= board.length || j<0 || j >= board[0].length ){
             throw new RuntimeException("Cell no valid");
         }
